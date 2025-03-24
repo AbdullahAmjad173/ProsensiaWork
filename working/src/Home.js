20e935c34d412b2822f01a23943e5ae91e4d93a7
Abdullah Amjad <abdullahamjad17301@gmail.com>
2025-03-24 12:47:29 +0500

    first commit

---
+++ working/src/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div
+      style={{
+        backgroundImage: "url('Prosensia_image.png')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        height: "100vh",
+        display: "flex",
+       
+       
+        color: "black",
+        
+      }}
+    >
+      <h1>Welcome to My Website</h1>
+    </div>
+  );
+};
+
+export default Home;
